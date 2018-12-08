10d2a5137c65ca5071e8cf0417449740008bbe32
刘晓杰 <1137800599@qq.com>
2018-12-08 21:47:35 +0800

    BindTest(加了点注释)

--- libapi/src/main/java/com/company/libapi/ActivityViewFinder.java
+++ libapi/src/main/java/com/company/libapi/ActivityViewFinder.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.view.View;
 
 
+/**
+ * 这个其实可以和ViewFinder写成一个文件，都是提供具体findViewById怎么实现的。之所以写成接口形式，估计是为了可拓展性
+ */
 public class ActivityViewFinder implements ViewFinder {
     @Override
     public View findView(Object object, int id) {
